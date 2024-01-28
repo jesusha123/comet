b3ea64a8de0717a6d39e102a4b055e4b4c5874aa
Jesus Herrera Arroyo <chuy.max@gmail.com>
2024-01-28 09:50:06 -0800

    Remove unused code

--- src/main_window.h
+++ src/main_window.h
@@ -4,7 +4,6 @@
 #include <QMainWindow>
 #include "response.h"
 #include "http_method.h"
-#include "request_storage.h"
 
 class HttpClient;
 class QTableWidgetItem;
@@ -25,8 +24,6 @@ public:
 
 private slots:
     void sendRequest();
-    void saveRequest();
-    void readRequest();
     void processResponse(const Response& response);
     void processParams(const QString& url);
     void processParamsChanged(QTableWidgetItem *item);
@@ -35,12 +32,10 @@ private slots:
     void processReqContentTypeChange(int index);
 
 private:
-    void initializeCollection();
     void initializeConnections();
     void buildParamsLineEdit();
 
     Ui::MainWindow *ui;
     HttpClient *httpClient;
-    RequestStorage requestStorage;
 };
 #endif // MAINWINDOW_H
