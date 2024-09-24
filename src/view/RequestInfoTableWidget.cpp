f082c2d4df439dba90bb2c66a1c2b6c22381f5d0
Jesus Herrera Arroyo <chuy.max@gmail.com>
2024-09-24 16:45:21 -0700

    Add header/param deletion button

--- src/view/RequestInfoTableWidget.cpp
+++ src/view/RequestInfoTableWidget.cpp
@@ -1,12 +1,28 @@
+#include <QPushButton>
 #include "RequestInfoTableWidget.h"
 
 RequestInfoTableWidget::RequestInfoTableWidget(QWidget *parent)
     : PropertyTableWidget(parent)
     , ignoreItemChanged(false)
 {
-    insertRow(0);
+    setColumnCount(3);
+    setHorizontalHeaderLabels({ "Key", "Value", "" });
 
     connect(this, &QTableWidget::itemChanged, this, &RequestInfoTableWidget::maintainBlankRowOnChange);
+    connect(model(), &QAbstractItemModel::rowsInserted, this, &RequestInfoTableWidget::onRowsInserted);
+
+    insertRow(0); // Call after setting onRowsInserted connection, so we add a delete button.
+}
+
+void RequestInfoTableWidget::addDeleteButtonToRow(int row)
+{
+    QPushButton *deleteButton = new QPushButton();
+    deleteButton->setIcon(QIcon::fromTheme("user-trash"));
+    deleteButton->setFlat(true);
+
+    connect(deleteButton, &QPushButton::clicked, this, &RequestInfoTableWidget::onDeleteButtonClicked);
+
+    setCellWidget(row, 2, deleteButton);
 }
 
 void RequestInfoTableWidget::maintainBlankRowOnChange(QTableWidgetItem *item)
@@ -19,6 +35,37 @@ void RequestInfoTableWidget::maintainBlankRowOnChange(QTableWidgetItem *item)
             ignoreItemChanged = true;
             insertRow(rowCount());
             ignoreItemChanged = false;
+        }
+    }
+}
+
+void RequestInfoTableWidget::onRowsInserted(const QModelIndex &parent, int first, int last)
+{
+    for (int row = first; row <= last; ++row)
+    {
+        addDeleteButtonToRow(row);
+    }
+}
+
+void RequestInfoTableWidget::onDeleteButtonClicked()
+{
+    QPushButton *button = qobject_cast<QPushButton *>(sender());
+    if (button)
+    {
+        int rowToDelete = -1;
+        for (int row = 0; row < rowCount(); ++row)
+        {
+            if (cellWidget(row, 2) == button)
+            {
+                rowToDelete = row;
+                break;
+            }
+        }
+
+        // Remove row only if we found a match, and if it's not the last row
+        if (rowToDelete != -1 && rowToDelete != rowCount()-1)
+        {
+            removeRow(rowToDelete);
         }
     }
 }
